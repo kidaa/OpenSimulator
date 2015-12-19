ad2f1fcbe864afa9ed8296a24d6a561f87aa692e
Christopher <christopher.latza@gmail.com>
2015-12-19 19:24:54 +0100

    update remote repo

--- sync.bat
+++ sync.bat
@@ -1,5 +1,5 @@
 @echo off
 git remote set-url origin git://opensimulator.org/git/opensim
 git pull origin master
-git remote set-url origin https://github.com/simpleSim/opensim.git
+git remote set-url origin https://github.com/ChrisWeymann/OpenSimulator.git
 git push origin master

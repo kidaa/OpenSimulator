29b1eef1e3b03bc124f0aa29d04ebf50edcc2188
Christopher <christopher.latza@gmail.com>
2015-12-21 16:44:51 +0100

    change version string

--- OpenSim/Framework/VersionInfo.cs
+++ OpenSim/Framework/VersionInfo.cs
@@ -51,7 +51,7 @@ namespace OpenSim
 
         public static string GetVersionString(string versionNumber, Flavour flavour)
         {
-            string versionString = "OpenSim " + versionNumber + " " + flavour;
+            string versionString = "FreeBird " + versionNumber;
             return versionString.PadRight(VERSIONINFO_VERSION_LENGTH);
         }
 

66a27884c467d031dd8c3ff0732469c4c5af4af8
Christopher <christopher.latza@gmail.com>
2015-12-25 15:35:56 +0100

    the jpeg delivery now works

--- addon-modules/OpenSim.Modules.JPEGConverter/src/JPEGConverterHandler.cs
+++ addon-modules/OpenSim.Modules.JPEGConverter/src/JPEGConverterHandler.cs
@@ -5,6 +5,8 @@ using System.Text;
 using OpenSim.Framework.Servers.HttpServer;
 using System.IO;
 using OpenSim.Framework;
+using OpenMetaverse;
+using System.Drawing;
 
 namespace OpenSim.Modules.JPEGConverter
 {
@@ -27,98 +29,101 @@ namespace OpenSim.Modules.JPEGConverter
             return bytes;
         }
 
+        private static byte[] ImageToByte(Image img)
+        {
+            ImageConverter converter = new ImageConverter();
+            return (byte[])converter.ConvertTo(img, typeof(byte[]));
+        }
+
+        public static byte[] ReadToEnd(System.IO.Stream stream)
+        {
+            long originalPosition = 0;
+
+            if (stream.CanSeek)
+            {
+                originalPosition = stream.Position;
+                stream.Position = 0;
+            }
+
+            try
+            {
+                byte[] readBuffer = new byte[4096];
+
+                int totalBytesRead = 0;
+                int bytesRead;
+
+                while ((bytesRead = stream.Read(readBuffer, totalBytesRead, readBuffer.Length - totalBytesRead)) > 0)
+                {
+                    totalBytesRead += bytesRead;
+
+                    if (totalBytesRead == readBuffer.Length)
+                    {
+                        int nextByte = stream.ReadByte();
+                        if (nextByte != -1)
+                        {
+                            byte[] temp = new byte[readBuffer.Length * 2];
+                            Buffer.BlockCopy(readBuffer, 0, temp, 0, readBuffer.Length);
+                            Buffer.SetByte(temp, totalBytesRead, (byte)nextByte);
+                            readBuffer = temp;
+                            totalBytesRead++;
+                        }
+                    }
+                }
+
+                byte[] buffer = readBuffer;
+                if (readBuffer.Length != totalBytesRead)
+                {
+                    buffer = new byte[totalBytesRead];
+                    Buffer.BlockCopy(readBuffer, 0, buffer, 0, totalBytesRead);
+                }
+                return buffer;
+            }
+            finally
+            {
+                if (stream.CanSeek)
+                {
+                    stream.Position = originalPosition;
+                }
+            }
+        }
+
         protected override byte[] ProcessRequest(string path, Stream requestData, IOSHttpRequest httpRequest, IOSHttpResponse httpResponse)
         {
             Dictionary<string, object> request = new Dictionary<string, object>();
             foreach (string name in httpRequest.QueryString)
                 request[name] = httpRequest.QueryString[name];
 
-            httpResponse.ContentType = "text/plain";
-
             if (m_JPEGConverter.AssetService != null)
             {
-                AssetBase asset = m_JPEGConverter.AssetService.Get(Convert.ToString(request["assetID"]));
+                string assetUUID = Convert.ToString(request["assetID"]);
 
-                if (asset != null)
+                if(m_JPEGConverter.AssetService.AssetsExist(new String[] {assetUUID})[0])
                 {
-                    try
+                    AssetBase asset = m_JPEGConverter.AssetService.Get(assetUUID);
+
+                    if (asset != null)
                     {
-                        if (asset.IsTextualAsset)
+                        if (asset.Type == (sbyte)AssetType.Texture)
                         {
                             OpenMetaverse.Imaging.ManagedImage jpegImageData = new OpenMetaverse.Imaging.ManagedImage(256, 256, OpenMetaverse.Imaging.ManagedImage.ImageChannels.Color);
 
-                            if (jpegImageData != null)
+                            if (OpenMetaverse.Imaging.OpenJPEG.DecodeToImage(asset.Data, out jpegImageData))
                             {
-                                if (OpenMetaverse.Imaging.OpenJPEG.DecodeToImage(asset.Data, out jpegImageData))
-                                {
-                                    Stream imageStream = new MemoryStream(jpegImageData.ExportRaw());
-                                    Stream saveStream = new MemoryStream();
+                                Stream imageStream = new MemoryStream(ImageToByte(jpegImageData.ExportBitmap()));
+                                Stream saveStream = new MemoryStream();
 
-                                    if (imageStream != null)
-                                    {
-                                        if (saveStream != null)
-                                        {
-                                            BinaryReader saveStreamReader = new BinaryReader(saveStream);
+                                System.Drawing.Image image = System.Drawing.Image.FromStream(imageStream);
+                                image.Save(saveStream, System.Drawing.Imaging.ImageFormat.Jpeg);
 
-                                            if (saveStreamReader != null)
-                                            {
-                                                System.Drawing.Image image = System.Drawing.Image.FromStream(imageStream);
-                                                image.Save(saveStream, System.Drawing.Imaging.ImageFormat.Jpeg);
-                                                httpResponse.ContentType = "image/jpeg";
-                                                return saveStreamReader.ReadBytes((int)saveStreamReader.BaseStream.Length);
-                                            }
-                                            else
-                                            {
-                                                m_JPEGConverter.Log.Error("saveStreamReader object is a null object");
-                                                return GetBytes("ERROR");
-                                            }
-                                        }
-                                        else
-                                        {
-                                            m_JPEGConverter.Log.Error("saveStream object is a null object");
-                                            return GetBytes("ERROR");
-                                        }
-                                    }
-                                    else
-                                    {
-                                        m_JPEGConverter.Log.Error("imageStream object is a null object");
-                                        return GetBytes("ERROR");
-                                    }
-                                }
-                                else
-                                {
-                                    m_JPEGConverter.Log.Error("ERROR AT DECODE IMAGE");
-                                    return GetBytes("ERROR");
-                                }
-                            }
-                            else
-                            {
-                                m_JPEGConverter.Log.Error("jpegImageData object is a null object");
-                                return GetBytes("ERROR");
+                                httpResponse.ContentType = "image/jpeg";
+                                return ReadToEnd(saveStream);
                             }
                         }
-                        else
-                        {
-                            return GetBytes("ERROR - NOT A TEXTUR");
-                        }
-                    }
-                    catch (Exception e)
-                    {
-                        m_JPEGConverter.Log.Error(e);
-                        return GetBytes(e.Message);
                     }
                 }
-                else
-                {
-                    m_JPEGConverter.Log.Error("AssetBase object is a null object");
-                    return GetBytes("ERROR");
-                }
             }
-            else
-            {
-                m_JPEGConverter.Log.Error("Asset server object is a null object");
-                return GetBytes("ERROR");
-            }
+
+            return null;
         }
     }
 }
